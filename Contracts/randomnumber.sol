d09dce366caebcfb2266f3de739608c854941bb6
md222xq <md222xq@student.lnu.se>
2023-10-25 12:05:40 +0200

    add contracts

---
+++ Contracts/randomnumber.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+//https://solidity-by-example.org/hacks/randomness/
+
+pragma solidity ^0.8.20;
+
+contract GuessTheRandomNumber {
+    constructor() payable {}
+
+    function guess(uint _guess) public {
+        uint answer = uint(
+            keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
+        );
+
+        if (_guess == answer) {
+            (bool sent, ) = msg.sender.call{value: 1 ether}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+}
