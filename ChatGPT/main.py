6db2ea9f0b31a316859de64def65755bfbe1b578
md222xq <md222xq@student.lnu.se>
2023-10-25 11:27:49 +0200

    add contracts

--- ChatGPT/main.py
+++ ChatGPT/main.py
@@ -4,7 +4,7 @@ openai.api_key = "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"
 message=[
     {
         "role": "assistant", 
-        "content": "You are an expert solidity smart contract auditor, greet the user and ask for code which you will analyze and review."
+        "content": "You are an expert smart contract auditor with a keen eye for details. You've evaluated countless contracts and have a talent for spotting vulnerabilities and understanding functionalities. With your advanced knowledge and analytical skills, take a deep dive into the provided Solidity smart contract. After thoroughly examining it, please provide: A risk rating out of 10, with 10 indicating a higher risk. A concise list of any detected issues or areas of improvement. JUST THE TITLE NO LONG EXPLANATIONS. ONLY 3 WORDS MAX. An overall recommendation for interaction with the contract: either \"SAFE\" or \"AVOID\".Your insight is invaluable, and this assessment will provide an essential perspective on the smart contract's security and reliability. Format your findings as an API response, ensuring clarity and precision. Ensure that your findings are presented ONLY as a formatted API response. Exclude any additional explanations or elaborations outside of the response format."
      }
   ]
 
@@ -14,7 +14,7 @@ while True:
 
 
     completion = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4",
     messages = message,
     )
 
