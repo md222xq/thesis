0183ca7ab24fa51ad12f0b3a9b346ffc8695ffea
Malek Darwish <md222xq@student.lnu.se>
2023-11-09 13:39:31 +0100

    update scripts

--- Testing/mini_experiment.py
+++ Testing/mini_experiment.py
@@ -5,32 +5,38 @@ import json
 from datetime import datetime
 from datetime import datetime
 from pathlib import Path
+import time
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
 # folder_path = './Contracts/Decompiled/Dedaub'
-folder_path = './Contracts/Decompiled/Heimdall-rs/'
+folder_path = './Contracts/Decompiled/Heimdall-rs/SmartBugs'
 gpt_model = "gpt-3.5-turbo-16k"
 # gpt_model = "gpt-4"
 
+test_type = 'heimdall'
+# test_type = 'dedaub'
+# test_type = 'source'
 
 
 
 
 
-def analyze_solidity_file(file_path, output_file, gpt_model):
+def analyze_solidity_file(file_path, output_file, gpt_model, test_run):
     with open(file_path, 'r', encoding='utf-8') as f:
         solidity_code = f.read()
 
     message = [
         {
             "role": "system",
-            "content": "you analyze smart contracts and respond in json format with Rating: SAFE or AVOID based on the smart contract. your response only contains the rating."
+            "content": "you analyze smart contracts for vulnrabilities and respond in json format with Rating: SAFE or Vulnrabased based on the smart contract. your response only contains the rating."
         },
         {
             "role": "user",
             "content": solidity_code
         }
     ]
+    print(json.dumps(message))
+    
     try:
 
         response = openai.ChatCompletion.create(
@@ -48,6 +54,7 @@ def analyze_solidity_file(file_path, output_file, gpt_model):
         output = ""
         for chunk in response:
             content = chunk.choices[0].delta.get('content', '')
+            print(content)
             output += content
 
         try:
@@ -59,6 +66,7 @@ def analyze_solidity_file(file_path, output_file, gpt_model):
 
 
         output_data = {
+                "test": test_run,
                 "file_path": file_path,
                 "analysis": analysis
             }
@@ -69,26 +77,35 @@ def analyze_solidity_file(file_path, output_file, gpt_model):
     except Exception as e:
         print(f"An error occurred while processing {file_path}: {str(e)}")
         output_data = {
+                "test": test_run,
                 "file_path": file_path,
                 "analysis": "FAILED to analyze, {e}"
             }
         json.dump(output_data, output_file, indent=2)
         output_file.write(',\n')
 
+
+
+
+
 output_dir = Path('test_results')
 output_dir.mkdir(exist_ok=True)  # Create the output directory if it doesn't exist
 
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-output_filename = output_dir / f'Test_{current_time}_{gpt_model}.json'  # Save the file in the output directory
+output_filename = output_dir / f'Test_{current_time}_{test_type}_{gpt_model}.json'  # Save the file in the output directory
 
 with open(output_filename, 'w', encoding='utf-8') as output_file:
     output_file.write('[\n') 
     # for root, dirs, files in os.walk(folder_path):
+    loop =1
     for file in os.listdir(folder_path):
         if file.endswith(".sol"):
             file_path = os.path.join(folder_path, file)
             print(file_path)
-            analyze_solidity_file(file_path, output_file, gpt_model)
+            analyze_solidity_file(file_path, output_file, gpt_model, loop)
+            # time.sleep(3)
+            loop +=1
+
     output_file.write('\n]')  
 
 print(f"Analysis results saved to {output_filename}")
